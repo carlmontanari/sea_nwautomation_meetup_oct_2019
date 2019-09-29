21b4fa2b7c3ecf8c134776285ad8b11873f247f5
carl <carl.r.montanari@gmail.com>
2019-09-29 10:06:23 -0700

    +x, update helper to reset failed hosts, preso updates

--- helper.py
+++ helper.py
@@ -5,6 +5,9 @@ def process_results(nr, task_result):
     """
     Print results of task if failed, skipped if skipped, success if... you get the point
 
+    Also resets any failed hosts... this is so the validate tasks continue even if we hit
+    an issue. Do not need to return this as we are modifying the nr object in memory.
+
     Arguments:
         nr: nornir object
         task_result: nornir aggregated result object
@@ -18,6 +21,7 @@ def process_results(nr, task_result):
     """
     if task_result.failed:
         print_result(task_result)
+        nr.data.reset_failed_hosts()
     elif task_result.name in nr.skip_tags:
         print(f"{task_result.name} SKIPPED!")
     else:
